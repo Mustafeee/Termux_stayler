c2f40e09614c738584f13cec810cf6460309f0f9
Mustafe <mustafeyare041@gmail.com>
2024-12-04 15:54:13 +0300

    Update termux_syler.sh

--- termux_syler.sh
+++ termux_syler.sh
@@ -1,115 +1,128 @@
 #!/bin/bash
 
-# Login Screen
-clear
-echo -e "\033[1;35mWelcome to POP-SMOKE Terminal \033[0m"
-echo -e "\033[1;33mPlease log in to proceed... \033[0m"
-sleep 1
+# Function to change Termux appearance
+change_appearance() {
+    clear
+    # Display welcome message with ASCII art
+    toilet -f slant "Welcome Back" -F gay
+    echo -e "\033[1;35mPOP-SMOKE Terminal - Customized! \033[0m"
+    echo -e "\033[1;33mSystem Initialized successfully. \033[0m"
+    sleep 2
+    clear
+}
 
-# Login Prompt
-echo -n "Username: "
-read username
-echo -n "Password: "
-read -s password
+# Function to change Termux color scheme
+change_colors() {
+    # Set custom color prompt
+    echo "PS1='\033[38;5;10m[\033[38;5;11m\u\033[38;5;10m@\033[38;5;12m\h\033[38;5;10m] \033[38;5;14m\w\033[38;5;10m$ \033[0m'" >> ~/.bashrc
+    source ~/.bashrc
+    echo -e "\033[1;32mPrompt color has been changed!\033[0m"
+}
 
-# Check login details (for demo, use static username/password)
-if [[ "$username" == "pop" && "$password" == "smoke123" ]]; then
-    echo -e "\n\033[1;32mLogin successful!\033[0m"
-    sleep 1
-else
-    echo -e "\n\033[1;31mIncorrect username or password.\033[0m"
-    exit 1
-fi
+# Function to change Termux font
+change_font() {
+    # Change the font style of Termux
+    echo -e "\033[1;36mChanging the font style...\033[0m"
+    pkg install -y termux-api
+    termux-reload-settings
+    echo -e "\033[1;32mFont has been changed to default.\033[0m"
+}
 
-# Customize Termux Appearance with ASCII Art
-clear
-toilet -f slant "CODED BY POP-SMOKE" -F gay
-echo -e "\033[1;36mCustomizing Termux... \033[0m"
-sleep 1
-clear
+# Function to change wallpaper
+change_wallpaper() {
+    clear
+    echo -e "\033[1;34mChanging wallpaper...\033[0m"
+    wget https://example.com/sunset.jpg -O ~/image.jpg
+    termux-wallpaper ~/image.jpg
+    echo -e "\033[1;32mWallpaper has been changed!\033[0m"
+}
 
-# Show System Info with Neofetch
-neofetch
+# Function to show a custom login message
+show_custom_message() {
+    clear
+    toilet -f term -F metal "Code By Pop-Smoke"
+    echo -e "\033[1;34mYour terminal is now customized!\033[0m"
+    sleep 2
+    clear
+}
 
-# Menu Options
-echo -e "\033[1;34mChoose an option: \033[0m"
-echo -e "\033[1;32m1. Update Termux \033[0m"
-echo -e "\033[1;33m2. Install Tools \033[0m"
-echo -e "\033[1;35m3. Start Python \033[0m"
-echo -e "\033[1;36m4. Show System Info \033[0m"
-echo -e "\033[1;31m5. Change Wallpaper \033[0m"
-echo -e "\033[1;37m6. Hack Mode \033[0m"
-echo -e "\033[1;32m7. Exit \033[0m"
-echo -n "Select an option: "
-read option
+# Function to open the YouTube link
+open_youtube() {
+    echo -e "\033[1;33mOpening YouTube Channel...\033[0m"
+    xdg-open "https://www.youtube.com/@XADAADI"
+    sleep 2
+}
 
-# Function-ka Doorashada
-case $option in
-    1)
-        # Update Termux
-        clear
-        figlet "Updating..." | toilet -F border
-        pkg update && pkg upgrade -y
-        echo -e "\033[1;32mTermux is up to date!\033[0m"
-        ;;
-    2)
-        # Install Essential Tools
-        clear
-        figlet "Installing..." | toilet -F border
-        pkg install git python curl neofetch cmatrix -y
-        echo -e "\033[1;32mEssential tools installed!\033[0m"
-        ;;
-    3)
-        # Start Python
-        clear
-        figlet "Python" | toilet -F border
-        echo -e "\033[1;36mStarting Python... \033[0m"
-        sleep 1
-        python
-        ;;
-    4)
-        # Show System Info
-        clear
-        figlet "System Info" | toilet -F border
-        neofetch
-        ;;
-    5)
-        # Change Wallpaper
-        clear
-        figlet "Wallpaper" | toilet -F border
-        echo -e "\033[1;34mChanging wallpaper... \033[0m"
-        echo "Choose wallpaper:"
-        echo "1. Sunset"
-        echo "2. Nature"
-        read wallpaper_choice
-        if [ $wallpaper_choice -eq 1 ]; then
-            wget https://example.com/sunset.jpg -O ~/image.jpg
-        else
-            wget https://example.com/nature.jpg -O ~/image.jpg
-        fi
-        termux-wallpaper ~/image.jpg
-        echo -e "\033[1;32mWallpaper changed!\033[0m"
-        ;;
-    6)
-        # Hack Mode
-        clear
-        figlet "Hack Mode" | toilet -F border
-        toilet -f term -F metal "Hacked!"
-        sleep 1
-        echo -e "\033[1;31mInitiating Hack Mode...\033[0m"
-        cmatrix -C red -s
-        ;;
-    7)
-        # Exit
-        clear
-        figlet "Goodbye" | toilet -F border
-        echo -e "\033[1;32mExiting... See you later! \033[0m"
-        exit 0
-        ;;
-    *)
-        # Invalid option
-        echo -e "\033[1;31mInvalid option. Try again.\033[0m"
-        sleep 1
-        exec $0
-        ;;
-esac
+# Function to display the main menu
+show_menu() {
+    clear
+    figlet "Main Menu" | toilet -F border
+    echo -e "\033[1;32m1. Update Termux \033[0m"
+    echo -e "\033[1;33m2. Install Custom Tools \033[0m"
+    echo -e "\033[1;34m3. Show System Info \033[0m"
+    echo -e "\033[1;35m4. Change Wallpaper \033[0m"
+    echo -e "\033[1;36m5. Change Termux Colors \033[0m"
+    echo -e "\033[1;31m6. Open YouTube Channel \033[0m"
+    echo -e "\033[1;31m7. Exit \033[0m"
+    echo -n "Select an option: "
+    read option
+    case $option in
+        1) update_termux ;;
+        2) install_tools ;;
+        3) show_system_info ;;
+        4) change_wallpaper ;;
+        5) change_colors ;;
+        6) open_youtube ;;
+        7) exit_script ;;
+        *) echo -e "\033[1;31mInvalid option. Please try again.\033[0m"; sleep 2; show_menu ;;
+    esac
+}
+
+# Function to update Termux
+update_termux() {
+    clear
+    figlet "Updating..." | toilet -F border
+    pkg update && pkg upgrade -y
+    echo -e "\033[1;32mTermux has been updated!\033[0m"
+    sleep 2
+    show_menu
+}
+
+# Function to install custom tools
+install_tools() {
+    clear
+    figlet "Installing..." | toilet -F border
+    pkg install git python curl neofetch cmatrix -y
+    echo -e "\033[1;32mTools have been installed!\033[0m"
+    sleep 2
+    show_menu
+}
+
+# Function to show system info
+show_system_info() {
+    clear
+    figlet "System Info" | toilet -F border
+    neofetch
+    sleep 2
+    show_menu
+}
+
+# Function to start Python
+start_python() {
+    clear
+    figlet "Python" | toilet -F border
+    python
+}
+
+# Function to exit script
+exit_script() {
+    clear
+    figlet "Goodbye" | toilet -F border
+    echo -e "\033[1;32mExiting Termux... See you later! \033[0m"
+    exit 0
+}
+
+# Start the script
+change_appearance
+show_custom_message
+show_menu
