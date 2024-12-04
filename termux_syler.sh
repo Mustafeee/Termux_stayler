256909c514993ca0378f5d6b31d33c2df166e004
Mustafe <mustafeyare041@gmail.com>
2024-12-04 15:37:51 +0300

    Update termux_syler.sh

--- termux_syler.sh
+++ termux_syler.sh
@@ -1,54 +1,66 @@
 #!/bin/bash
 
-# Dhisida ASCII art Intro
+# Function-ka Login
+clear
+echo -e "\033[1;35mWelcome to POP-SMOKE Tool \033[0m"
+echo -e "\033[1;33mPlease log in to proceed... \033[0m"
+sleep 1
+
+# Login Prompt
+echo -n "Username: "
+read username
+echo -n "Password: "
+read -s password
+
+# Check login details (for demo, use static username/password)
+if [[ "$username" == "pop" && "$password" == "smoke123" ]]; then
+    echo -e "\n\033[1;32mLogin successful!\033[0m"
+    sleep 1
+else
+    echo -e "\n\033[1;31mIncorrect username or password.\033[0m"
+    exit 1
+fi
+
+# ASCII Art and Setup Termux Appearance
 clear
 toilet -f slant "CODED BY POP-SMOKE" -F gay
-echo ""
-echo -e "\033[1;32m WELCOME TO POP-SMOKE TOOL \033[0m"
-echo -e "\033[1;35m Let's make Termux more fun! \033[0m"
+echo -e "\033[1;36mCustomizing Termux... \033[0m"
 sleep 1
 clear
 
-# Qurxinta ASCII Art loading effect
-echo -e "\033[1;33m Loading... \033[0m"
-cmatrix -C green -s -u 10
-clear
-
-# Menu Options
-echo -e "\033[1;36m1. Update Termux \033[0m"
-echo -e "\033[1;32m2. Install Essential Tools \033[0m"
-echo -e "\033[1;34m3. Start Python \033[0m"
-echo -e "\033[1;31m4. Show System Info \033[0m"
-echo -e "\033[1;35m5. Change Wallpaper \033[0m"
+# Display the Menu
+echo -e "\033[1;34mChoose an option: \033[0m"
+echo -e "\033[1;32m1. Update Termux \033[0m"
+echo -e "\033[1;33m2. Install Tools \033[0m"
+echo -e "\033[1;35m3. Start Python \033[0m"
+echo -e "\033[1;36m4. Show System Info \033[0m"
+echo -e "\033[1;31m5. Change Wallpaper \033[0m"
 echo -e "\033[1;37m6. Hack Mode \033[0m"
-echo -e "\033[1;33m7. Exit \033[0m"
-echo ""
-echo -n "Choose an option: "
+echo -e "\033[1;32m7. Exit \033[0m"
+echo -n "Select an option: "
 read option
 
-# Function-ka doorashada
+# Function-ka Doorashada
 case $option in
     1)
         # Update Termux
         clear
         figlet "Updating..." | toilet -F border
         pkg update && pkg upgrade -y
-        echo -e "\033[1;32m Termux has been updated successfully! \033[0m"
-        sleep 1
+        echo -e "\033[1;32mTermux is up to date!\033[0m"
         ;;
     2)
         # Install Essential Tools
         clear
         figlet "Installing..." | toilet -F border
-        pkg install git python curl nano neofetch cmatrix -y
-        echo -e "\033[1;32m Essential tools have been installed! \033[0m"
-        sleep 1
+        pkg install git python curl neofetch cmatrix -y
+        echo -e "\033[1;32mEssential tools installed!\033[0m"
         ;;
     3)
         # Start Python
         clear
         figlet "Python" | toilet -F border
-        echo -e "\033[1;36m Starting Python... \033[0m"
+        echo -e "\033[1;36mStarting Python... \033[0m"
         sleep 1
         python
         ;;
@@ -62,7 +74,7 @@ case $option in
         # Change Wallpaper
         clear
         figlet "Wallpaper" | toilet -F border
-        echo -e "\033[1;34m Changing wallpaper... \033[0m"
+        echo -e "\033[1;34mChanging wallpaper... \033[0m"
         echo "Choose wallpaper:"
         echo "1. Sunset"
         echo "2. Nature"
@@ -73,7 +85,7 @@ case $option in
             wget https://example.com/nature.jpg -O ~/image.jpg
         fi
         termux-wallpaper ~/image.jpg
-        echo -e "\033[1;32m Wallpaper changed successfully! \033[0m"
+        echo -e "\033[1;32mWallpaper changed!\033[0m"
         ;;
     6)
         # Hack Mode
@@ -81,21 +93,19 @@ case $option in
         figlet "Hack Mode" | toilet -F border
         toilet -f term -F metal "Hacked!"
         sleep 1
-        echo -e "\033[1;31m Initiating Hacking Mode... \033[0m"
+        echo -e "\033[1;31mInitiating Hack Mode...\033[0m"
         cmatrix -C red -s
         ;;
     7)
         # Exit
         clear
         figlet "Goodbye" | toilet -F border
-        echo -e "\033[1;32m Exiting... Termux ka bixi. \033[0m"
-        sleep 1
+        echo -e "\033[1;32mExiting... See you later! \033[0m"
         exit 0
         ;;
     *)
         # Invalid option
-        clear
-        echo -e "\033[1;31m Invalid option. Please try again. \033[0m"
+        echo -e "\033[1;31mInvalid option. Try again.\033[0m"
         sleep 1
         exec $0
         ;;
